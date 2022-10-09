266f11d97fa446063de346d6b31dcb4d3912aad6
vibakar <vibakar23@gmail.com>
2022-10-09 12:30:30 +0100

    Added Operators

---
+++ 1-Python-Comparison-Operators/1-operators.py
@@ -0,0 +1,29 @@
+# Equal
+print(2 == 2)
+print(1 == 0)
+
+# Note that == is a comparison operator, while = is an assignment operator.
+# Not Equal
+print(2 != 1)
+print(2 != 2)
+
+# Greater Than
+print(2 > 1)
+print(2 > 4)
+
+# Less Than
+print(2 < 4)
+print(2 < 1)
+
+# Greater Than or Equal to
+print(2 >= 2)
+print(2 >= 1)
+
+# Less than or Equal to
+print(2 <= 2)
+print(2 <= 4)
+
+print("###########################")
+print(1 < 2 < 3)
+print(1<3 and 3>2)
+print(1==1 or 100==1)
