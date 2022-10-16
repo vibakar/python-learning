7e66bea2c9472e0be5c8676b6a7f5e03477c5de0
vibakar <vibakar23@gmail.com>
2022-10-16 11:17:24 +0100

    Added color to the tic tac toe board

--- 4-Milestone-1/tic-tac-toe.py
+++ 4-Milestone-1/tic-tac-toe.py
@@ -4,48 +4,51 @@ row3 = [' ', ' ', ' ']
 
 board = [row1, row2, row3]
 
-color = {"blue": "\033[94m", "pink": "\033[90m", "red": "\033[91m", "end": "\033[0m"}
+color = {"message": "\033[90m", "error": "\033[91m", "Player1": "\033[94m", "Player2" : "\033[95m", "bold": "\033[1m", "end": "\033[0m"}
 
 def reset_board():
     for index,x in enumerate(board):
         for innerIndex,y in enumerate(x):
             board[index][innerIndex] = ' '
 
-def display_board(rows): 
+def text_color(symbol): 
+    return (color['Player2'] + symbol + color['end'],color['Player1'] + symbol + color['end'])[symbol == 'X']
+
+def display_board(): 
     print('-------------')
     print('|   |   |   |')
-    print('| ' + board[0][0] + ' | ' + board[0][1] + ' | ' + board[0][2] + ' | ')
+    print('| ' + text_color(board[0][0]) + ' | ' + text_color(board[0][1]) + ' | ' + text_color(board[0][2]) + ' | ')
     print('|   |   |   |')
     print('-------------')
     print('|   |   |   |')
-    print('| ' + board[1][0] + ' | ' + board[1][1] + ' | ' + board[1][2] + ' | ')
+    print('| ' + text_color(board[1][0]) + ' | ' + text_color(board[1][1]) + ' | ' + text_color(board[1][2]) + ' | ')
     print('|   |   |   |')
     print('-------------')
     print('|   |   |   |')
-    print('| ' + board[2][0] + ' | ' + board[2][1] + ' | ' + board[2][2] + ' | ')
+    print('| ' + text_color(board[2][0]) + ' | ' + text_color(board[2][1]) + ' | ' + text_color(board[2][2]) + ' | ')
     print('|   |   |   |')
     print('-------------')
 
-def get_position(arg):
+def get_position(type, player):
     position = "WRONG"
     within_range = False
     empty_row = False
-    input_msg = f"{color['blue']}Select your {arg}: [1, 2, 3] {color['end']}"
+    input_msg = f"{color[player]}Select your {type}: [1, 2, 3] {color['end']}"
 
     while position.isdigit() == False or within_range == False or empty_row == False:
         position = input(input_msg)
 
         if position.isdigit() == False:
-            print(f"{color['red']}Sorry, please enter a valid {arg} : [1, 2, 3] {color['end']}")
+            print(f"{color['error']}Sorry, please enter a valid {type} : [1, 2, 3] {color['end']}")
         
         if position.isdigit() == True:
             if int(position) not in [1, 2, 3]:
-                print(f"{color['red']}Please choose the {arg} between 1 and 3 {color['end']}")
+                print(f"{color['error']}Please choose the {type} between 1 and 3 {color['end']}")
                 within_range = False
             else:
                 within_range = True
-                if arg == "row" and ' ' not in board[int(position) - 1]:
-                    print(f"{color['red']}No empty field in the selected {arg}. Try again.. {color['end']}")
+                if type == "row" and ' ' not in board[int(position) - 1]:
+                    print(f"{color['error']}No empty field in the selected {type}. Try again.. {color['end']}")
                     empty_row = False
                 else:
                     empty_row = True
@@ -71,15 +74,15 @@ def check_for_win(symbol):
     else:
         return False
     
-def validate_selection(row, index):
+def validate_selection(row, index, player):
     invalid_index = False
 
     if board[row - 1][index - 1] != ' ':
         invalid_index = True
 
     while invalid_index == True:
-        print(f"{color['red']}You can't choose this position. Try different position. {color['end']}")
-        index = get_position("position")
+        print(f"{color['error']}You can't choose this position. Try different position. {color['end']}")
+        index = get_position("position", player)
         if board[row - 1][index - 1] != ' ':
             invalid_index = True
         else:
@@ -88,10 +91,10 @@ def validate_selection(row, index):
 
 def verify_move(player, symbol):
     if check_for_win(symbol) == True:
-        print(f"{color['pink']}{player[symbol]} won the game!!! {color['end']}")
+        print(f"{color['bold']}{color['message']}Congratulations!!! {player[symbol]} won the game!!! {color['end']}")
         return True
     elif (' ' not in row1) and (' ' not in row2) and (' ' not in row3) :
-        print(f"{color['pink']}Match ended in Draw!!! {color['end']}")            
+        print(f"{color['bold']}{color['message']}Match ended in Draw!!! {color['end']}")            
         return True
     else:
         return False
@@ -103,34 +106,34 @@ def get_user_input():
     symbol = "X"
 
     while match_ended == False:
-        if count%2 == 0: symbol = "X"; print(f"{color['blue']}{player[symbol]}'s Turn:{color['end']}")
-        else: symbol = "Y"; print(f"{color['blue']}{player[symbol]}'s Turn:{color['end']}")
+        if count%2 == 0: symbol = "X"; print(f"{color['Player1']}{player[symbol]}'s Turn:{color['end']}")
+        else: symbol = "Y"; print(f"{color['Player2']}{player[symbol]}'s Turn:{color['end']}")
 
-        row_val = get_position("row")
-        index_val = get_position("index")
-        row, index = validate_selection(row_val, index_val)
+        row_val = get_position("row", player[symbol])
+        index_val = get_position("index", player[symbol])
+        row, index = validate_selection(row_val, index_val, player[symbol])
 
         board[row - 1 ][index - 1] = symbol
-        display_board(board)
+        display_board()
         match_ended = verify_move(player, symbol)
         count += 1
 
 def play():
-    print(f"{color['blue']}Welcome to game: Tic Tac Toe!!! {color['end']}")
+    print(f"{color['bold']}{color['message']}Welcome to the game: Tic Tac Toe!!! {color['end']}")
     play_game = "WRONG"
 
     while play_game not in ['Y', 'N', 'y', 'n']:
-        play_game = input(f"{color['blue']}Would you like to start a new game?: ['Y', 'N'] {color['end']}")
+        play_game = input(f"{color['message']}Would you like to start a new game?: ['Y', 'N'] {color['end']}")
 
         if play_game not in ['Y', 'N', 'y', 'n']:
-            print(f"{color['red']}Invalid input. Try again... Choose either Y or N{color['end']}")
+            print(f"{color['error']}Invalid input. Try again... Choose either Y or N{color['end']}")
 
         if play_game in ["Y", "y"]:
-            display_board(board)
+            display_board()
             get_user_input()
             reset_board()
             play_game = "WRONG"
         elif play_game in ["N", "n"]:
-            print(f"{color['blue']}Bye.. See you here soon!!!{color['end']}")
+            print(f"{color['message']}Bye.. See you here soon!!!{color['end']}")
 
 play()
